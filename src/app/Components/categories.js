a2203577b5a9f48848584d016d5aa789c9ecdc40
skashequr <learningwithextracare@gmail.com>
2024-02-23 03:26:57 +0600

    first commit

---
+++ src/app/Components/categories.js
@@ -0,0 +1,23 @@
+"use client";
+const Categories = () => {
+  return (
+    <div className="bg-white">
+      <div className="p-4 bg-[#1FA45B] rounded rounded-t-md">
+        <h6 className="text-center">Categories</h6>
+      </div>
+      <div className="p-3 relative mx-auto text-gray-600">
+        <input
+          className="border-2 border-gray-300 w-full bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          type="search"
+          name="search"
+          placeholder="Search"
+        />
+        <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 4a6 6 0 1 0 0 12 6 6 0 0 0 0-12zm-8 6a8 8 0 1 1 14.32 4.906l5.387 5.387a1 1 0 0 1-1.414 1.414l-5.387-5.387A8 8 0 0 1 2 10z" fill="#0D0D0D"/></svg>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Categories;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import keepPreset from "keep-react/preset";
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/keep-react/**/*.{js,jsx,ts,tsx}",
   ],
+  presets: [keepPreset],
   theme: {
     extend: {
       backgroundImage: {
@@ -16,3 +19,6 @@ module.exports = {
   },
   plugins: [],
 };
+
+
+
